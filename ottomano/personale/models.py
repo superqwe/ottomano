db0599530b9a9daf096965dc024f70fddcc81be6
Leonardo Masi <leonardomasi@tin.it>
2022-02-17 15:47:42 +0100

    registrazione app personale

--- ottomano/personale/models.py
+++ ottomano/personale/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Lavoratore(models.Model):
+    cognome = models.CharField(max_length=20)
+    nome = models.CharField(max_length=20)
