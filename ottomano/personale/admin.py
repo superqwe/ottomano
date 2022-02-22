90b0e4a03cb890e1a1002cfc47d76ba148d68878
Leonardo Masi <leonardomasi@tin.it>
2022-02-22 16:00:18 +0100

    modifiche admin

--- ottomano/personale/admin.py
+++ ottomano/personale/admin.py
@@ -7,8 +7,10 @@ from .models import Lavoratore
 
 class LavoratoreAdmin(admin.ModelAdmin):
     list_display = (
-    'in_forza', 'cognome', 'nome', 'cellulare', 'data_fine', 'qualifica', 'assunzione', 'mansione', 'attivita_svolta',
-    'mansione_1', 'mansione_2', 'mansione_3', 'reparto')
+        'in_forza', 'cognome', 'nome', 'cellulare', 'data_fine', 'qualifica', 'assunzione', 'mansione',
+        'attivita_svolta', 'mansione_1', 'mansione_2', 'mansione_3', 'reparto')
+    list_display_links = ('cognome', 'nome')
+    list_filter =('in_forza', 'reparto')
 
 
 admin.site.register(Lavoratore, LavoratoreAdmin)
