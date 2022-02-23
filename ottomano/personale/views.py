cf799c6f5b0798b1f4ca2c008a0459a8908622e9
Leonardo Masi <leonardomasi@tin.it>
2022-02-23 10:28:32 +0100

    aggiorna_documenti: ricerca nuovi lavoratori

--- ottomano/personale/views.py
+++ ottomano/personale/views.py
@@ -1,11 +1,15 @@
+import os
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-import personale.importa_dati
+# import personale.importa_dati
 
 from personale.models import Lavoratore, Formazione
+
+PATH_DOCUMENTI = r'C:\Users\benedetto.basile\Dropbox\Documenti_Lavoratori'
 
 
 def index(request):
@@ -32,10 +36,29 @@ def formazione(request):
 
     return render(request, 'personale/formazione.html', context)
 
+
 def aggiorna_documenti(request):
-    formazione = Formazione.objects.all()
-    print(formazione)
-    context = {'titolo': 'Formazione',
-               'formazione': formazione}
+    # verifica se sono presenti nuovi lavoratori
+    elenco_lavoratori = Lavoratore.objects.all().values_list('cognome', 'nome')
+    elenco_lavoratori = [('%s %s' % lavoratore).lower() for lavoratore in elenco_lavoratori]
+
+    dir_lavoratori = os.listdir(PATH_DOCUMENTI)
+    dir_lavoratori = [lavoratore.lower() for lavoratore in dir_lavoratori]
+
+    nuovi_lavoratori=[]
+    for lavoratore in dir_lavoratori:
+        if not lavoratore in elenco_lavoratori:
+            if os.path.isdir(lavoratore):
+                nuovi_lavoratori.append(lavoratore)
+                print('\n'*3, 'Nuovo lavoratore', lavoratore, '\n'*3)
+                #todo: implementare nuovo lavoratore
+
+
+    ####
+    # formazione = Formazione.objects.all()
+    # print(formazione)
+
+    context = {'titolo': 'Aggiorna Documenti',
+               'nuovi_lavoratori': nuovi_lavoratori}
 
     return render(request, 'personale/aggiorna_documenti.html', context)
