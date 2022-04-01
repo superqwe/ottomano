be24ab1e419fcec2b55ff68daab70d00fb2af1a3
Leonardo Masi <leonardomasi@tin.it>
2022-04-01 14:47:51 +0200

    link idonità

--- ottomano/ottomano/settings.py
+++ ottomano/ottomano/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    r'C:\Users\benedetto.basile\Dropbox\Documenti_Lavoratori',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
