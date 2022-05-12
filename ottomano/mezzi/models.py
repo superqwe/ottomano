af1746f0f176bc873737354a1c2cf71c1b8de22b
Leonardo Masi <leonardomasi@tin.it>
2022-05-12 14:45:45 +0200

    mezzi matricola_inail aumentata

--- ottomano/mezzi/models.py
+++ ottomano/mezzi/models.py
@@ -43,7 +43,7 @@ class Mezzo(models.Model):
     libretto = models.BooleanField(default=None)
     revisione = models.DateField(blank=True, null=True)
     immatricolazione = models.DateField(blank=True, null=True)
-    matricola_inail = models.CharField(max_length=30, blank=True, null=True, default=None)
+    matricola_inail = models.CharField(max_length=40, blank=True, null=True, default=None)
     libretto_inail = models.BooleanField(default=False)
     inail = models.DateField(blank=True, null=True)
     manuale = models.BooleanField(default=False)
