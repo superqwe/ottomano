7e382e338ae7b1ecce0ea0bde71ce6e92867b415
superqwe <leonardomasi@tin.it>
2023-06-16 15:33:16 +0200

    Layout admin

--- ottomano/sgi/admin.py
+++ ottomano/sgi/admin.py
@@ -5,6 +5,7 @@ from .models import Formazione
 
 class FormazioneAdmin(admin.ModelAdmin):
     list_display = ('mese', 'corso', 'data', 'argomento', 'docente', 'ore', 'persone')
+    date_hierarchy = 'data'
     save_on_top = True
 
 
