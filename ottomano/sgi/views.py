7af20cd452742215790277529a978f962633fde9
superqwe <leonardomasi@tin.it>
2023-11-08 15:55:00 +0100

    Formazione: statistiche

--- ottomano/sgi/views.py
+++ ottomano/sgi/views.py
@@ -1,14 +1,23 @@
 import csv
 from datetime import datetime
 
+from django.db.models import Max
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from personale.models import Lavoratore
 from sgi.models import Formazione, Non_Conformita
 
 from pprint import pprint as pp
 
 FORMAZIONE_ANNO = 2023
+FORMAZIONE_FRAZIONI_ORE = {
+    '10min': 1 / 6,
+    '15min': 0.25,
+    '30min': 0.5,
+    '1.5': 1.5,
+    '1,5': 1.5,
+}
 
 
 def index(request):
@@ -50,10 +59,44 @@ def index(request):
 def formazione(request, anno=FORMAZIONE_ANNO):
     formazione_ = Formazione.objects.filter(data__year=anno)
 
+    # sessioni
+    corsi_totali = formazione_.count()
+    corsi_esterni = formazione_.filter(corso=None).count()
+    corsi_interni = corsi_totali - corsi_esterni
+
+    # ore
+    ore_interno = ore_esterno = 0
+    for corso in formazione_:
+        if corso.ore.isnumeric():
+            ore = int(corso.ore) * int(corso.persone)
+        else:
+            ore = FORMAZIONE_FRAZIONI_ORE[corso.ore] * int(corso.persone)
+
+        if corso.corso:
+            ore_interno += ore
+        else:
+            ore_esterno += ore
+
+    ore_totali = ore_interno + ore_esterno
+
+    # ore media persona
+    n_lavoratori = Lavoratore.objects.filter(in_forza=True).count()
+    media_interna = ore_interno / n_lavoratori
+    media_esterna = ore_esterno / n_lavoratori
+    media_totale = ore_totali / n_lavoratori
+
+    # statistiche ore
+    statistiche = (
+        ('Corsi interni', corsi_interni, ore_interno, media_interna),
+        ('Corsi esterni', corsi_esterni, ore_esterno, media_esterna),
+        ('Totale', corsi_totali, ore_totali, media_totale)
+    )
+
     context = {'titolo': 'Programma di informazione, formazione, addestramento',
                'pagina_attiva_formazione': 'active',
                'sezione_sgi_attiva': 'active',
                'formazione': formazione_,
+               'statistiche': statistiche
                }
 
     pagina_attiva_formazione_2023 = pagina_attiva_formazione_2022 = ''
@@ -68,6 +111,8 @@ def formazione(request, anno=FORMAZIONE_ANNO):
     context['pagina_attiva_formazione_2022'] = pagina_attiva_formazione_2022
 
     return render(request, 'sgi/formazione.html', context)
+
+
 def non_conformita(request, anno=FORMAZIONE_ANNO):
     non_conformita_ = Non_Conformita.objects.filter(data__year=anno).order_by('-data')
 
