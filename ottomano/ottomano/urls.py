4c35daf7bf6db7cef534ddfa60a155e2b011098f
superqwe <leonardomasi@tin.it>
2023-11-30 15:51:16 +0100

    estrai_dati: layout

--- ottomano/ottomano/urls.py
+++ ottomano/ottomano/urls.py
@@ -14,16 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-###
-from django.contrib import admin
 from django.urls import include, path
+
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
