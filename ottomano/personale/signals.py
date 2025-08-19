1f3ef5552484649733400f6eae60314b377c3cc3
Leonardo Masi <superqwe@users.noreply.github.com>
2025-08-19 09:23:34 +0200

    Crea la cartella del lavoratore quando ne viene creato uno nuovo

--- ottomano/personale/signals.py
+++ ottomano/personale/signals.py
@@ -1,9 +1,23 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Lavoratore, Formazione, Idoneita
+from icecream import ic
+from .views import PATH_DOCUMENTI
+from pathlib import Path
+
 
 @receiver(post_save, sender=Lavoratore)
 def crea_formazione_idoneita(sender, instance, created, **kwargs):
     if created:
         Formazione.objects.create(lavoratore=instance)
         Idoneita.objects.create(lavoratore=instance)
+
+        cartella_lavoratore = Path(PATH_DOCUMENTI) / f'{instance.cognome.upper()} {instance.nome}'
+        cartelle = (cartella_lavoratore,
+                    cartella_lavoratore / 'attestati',
+                    cartella_lavoratore / 'nomine',
+                    cartella_lavoratore / 'superati',
+                    cartella_lavoratore / 'vari',
+                    )
+        for cartella in cartelle:
+            cartella.mkdir(parents=True, exist_ok=True)
